cc22d5469cbf1ecce90774bf60d9ec29332e0374
Snehil Seenu <80764244+Mijokotsi@users.noreply.github.com>
2022-04-22 12:01:15 +0530

    Update chat.py

--- ChatBotFinalProject/chat.py
+++ ChatBotFinalProject/chat.py
@@ -1,8 +1,12 @@
+#Implementing chat on terminal
+
 import random
 import json
 import torch
+import pywhatkit as pwt
 from model import NeuralNet
 import nltk_utils
+from subprocess import call
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -23,14 +27,45 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
+# creating class for importing the image file
+class CallImage(object):
+    
+    # defining path of the image file
+    def __init__(self, path=r"C:\Users\Snehil Seenu\PycharmProjects\ChatBotFinalProject\image.py"):
+        self.path = path
+
+    def call_image(self):
+        call(["Python", "{}".format(self.path)])
+
+
 bot_name = "Shein"
-print("Let's Chat! (type 'quit' to exit)")
+print("Let's Chat! (type 'quit'/'exit' to exit)")
 while True:
     # sentence = "What can you search for us?"
     sentence = input("You: ")
-    if sentence == "quit":
-        break
 
+    if sentence == "google" or sentence == "Google" or sentence == "search on google":
+        sentence = input("What do you want to search: ")
+        pwt.search(sentence)
+        continue
+         
+    # If the user types download, image file runs
+    if sentence == "download":
+        print("Image Downloader opening...")
+        c = CallImage()
+        c.call_image()
+        continue
+
+    if sentence == 'youtube' or sentence == "search on youtube":
+        sentence = input("What do you want to see: ")
+        print("playing...")
+        pwt.playonyt(sentence)
+        continue
+
+    if sentence == "quit" or sentence == "exit":
+        break
+        
+    # And the NLP processing starts
     sentence = nltk_utils.tokenize(sentence)
     X = nltk_utils.bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
